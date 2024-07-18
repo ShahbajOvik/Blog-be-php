cd55acd80488da7282dd94558e77982de081f847
ShahbajOvik <ovikkhan420@gmail.com>
2024-07-18 03:17:53 -0700

    SlideBar Updated

--- post.php
+++ post.php
@@ -13,15 +13,15 @@
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 			<div class="about">
-				<?php
+			<?php
 					$query ="select * from tbl_post where id=$id";
 					$post = $db->select($query);
 
-			if($post)
-			{
+				if($post)
+				{
 				while ($result = $post ->fetch_assoc())
 				{
-				?>
+			?>
 				<h2><?php echo $result['title'];?></h2>
 				<h4><?php echo $fm->formatDate($result['date']);?> <a href="#"><?php echo $result['author'];?></a></h4>
 				<img src="admin/upload/<?php echo $result['image'];?>" alt="post image"/>
